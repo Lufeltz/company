5516070fd71324d04503565de8c31866e764d7f0
Vinicius Savitraz <savitraz@Viniciuss-MacBook-Pro.local>
2024-09-26 11:04:24 -0300

    R15: Cadastro de Voo

--- src/app/funcionario/r15-cadastro-de-voo/r15-cadastro-de-voo.component.ts
+++ src/app/funcionario/r15-cadastro-de-voo/r15-cadastro-de-voo.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { VoosService } from '../../services/prototipo/voos.service';
 import { CommonModule } from '@angular/common';
+import {LetrasSomenteDirective} from "../../shared/directives/letras-somente.directive";
 
 @Component({
   selector: 'app-r15-cadastro-de-voo',
   standalone: true,
-  imports: [CommonModule],
+    imports: [CommonModule, LetrasSomenteDirective],
   templateUrl: './r15-cadastro-de-voo.component.html',
   styleUrl: './r15-cadastro-de-voo.component.css'
 })
