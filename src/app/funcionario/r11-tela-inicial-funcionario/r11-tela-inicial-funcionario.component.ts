b1ad5ec139adb81d3b395414af45e0cca97758c5
Julio Yamaguchi <julioyamaguchi@ufpr.br>
2024-10-13 18:37:32 -0300

    tela r12 finalizada

--- src/app/funcionario/r11-tela-inicial-funcionario/r11-tela-inicial-funcionario.component.ts
+++ src/app/funcionario/r11-tela-inicial-funcionario/r11-tela-inicial-funcionario.component.ts
@@ -2,18 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { VoosService } from '../../services/prototipo/voos.service';
 import { CommonModule } from '@angular/common';
 import { Voo } from '../../shared/models/prototipo/voo.model';
+import { RouterModule } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { R12ConfirmacaoEmbarqueComponent } from '../r12-confirmacao-embarque/r12-confirmacao-embarque.component';
 
 @Component({
   selector: 'app-r11-tela-inicial-funcionario',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './r11-tela-inicial-funcionario.component.html',
   styleUrl: './r11-tela-inicial-funcionario.component.css',
 })
 export class R11TelaInicialFuncionarioComponent implements OnInit {
   public voos: Voo[] = [];
 
-  constructor(private voosService: VoosService) {}
+  constructor(
+    private voosService: VoosService,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
     this.getAllvoos();
@@ -32,5 +38,12 @@ export class R11TelaInicialFuncionarioComponent implements OnInit {
         console.log('Erro ao carregar voos');
       },
     });
+  }
+
+  //AÇÕES DOS BOTÕES
+
+  abrirModalConfirmacaoEmbarque(voo: Voo) {
+    const modalRef = this.modalService.open(R12ConfirmacaoEmbarqueComponent);
+    modalRef.componentInstance.vooRecebido = voo;
   }
 }
--- src/app/funcionario/r12-confirmacao-embarque/r12-confirmacao-embarque.component.ts
+++ src/app/funcionario/r12-confirmacao-embarque/r12-confirmacao-embarque.component.ts
@@ -1,16 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { VoosService } from '../../services/prototipo/voos.service';
 import { CommonModule } from '@angular/common';
+import { ReservasService } from '../../services/prototipo/reservas.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Voo } from '../../shared/models/prototipo/voo.model';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Reserva } from '../../shared/models/prototipo/reserva.model';
 
 @Component({
   selector: 'app-r12-confirmacao-embarque',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './r12-confirmacao-embarque.component.html',
-  styleUrl: './r12-confirmacao-embarque.component.css'
+  styleUrl: './r12-confirmacao-embarque.component.css',
 })
-export class R12ConfirmacaoEmbarqueComponent {
+export class R12ConfirmacaoEmbarqueComponent implements OnInit {
+  @Input() vooRecebido!: Voo;
+  @ViewChild('formCodigoreserva') formCodigoReserva!: NgForm;
+
+  ngOnInit() {}
+
+  private reservas: Reserva[] = [];
+  codigoReserva: string = '';
+  reservaNaoEncontrada: boolean = false;
+  reservaEncontrada: boolean = false;
+  reserva!: Reserva;
+
   constructor(
-    private voos: VoosService
+    private voosService: VoosService,
+    private reservaService: ReservasService,
+    public activeModal: NgbActiveModal
   ) {}
+
+  consultarReserva() {
+    this.reservaService.getAllReservas().subscribe({
+      next: (data: Reserva[] | null) => {
+        if (data == null) {
+          this.reservas = [];
+        } else {
+          this.reservas = data;
+
+          // Filtra a lista de reservas para encontrar a reserva com o código digitado
+          const reservaEncontradanaLista = this.reservas.find(
+            (reserva) => reserva.codigoReserva === this.codigoReserva
+          );
+
+          // Verifica se a reserva foi encontrada
+          if (reservaEncontradanaLista) {
+            // Verifica se o código do voo na reserva corresponde ao voo recebido
+            if (
+              reservaEncontradanaLista.codigoVoo === this.vooRecebido.codigoVoo
+            ) {
+              this.reserva = reservaEncontradanaLista;
+              this.reservaEncontrada = true;
+              this.reservaNaoEncontrada = false;
+            } else {
+              // A reserva não pertence ao voo recebido
+              this.reservaNaoEncontrada = true;
+              this.reservaEncontrada = false;
+            }
+          } else {
+            // Nenhuma reserva com o código foi encontrada
+            this.reservaNaoEncontrada = true;
+            this.reservaEncontrada = false;
+          }
+        }
+      },
+      error: (err) => {
+        console.log('Erro ao carregar reservas da base de dados:', err);
+      },
+    });
+  }
+
+  confirmaReserva() {
+    this.reserva.estadoReserva = 'embarcado';
+    this.reservaService.putReserva(this.reserva).subscribe({
+      next: (response) => {
+        if (response) {
+          alert(
+            'Reserva alterada com sucesso! Status da Reserva: ' +
+              this.reserva.estadoReserva
+          );
+          this.activeModal.close();
+        } else {
+          console.log('Falha ao criar a reserva.');
+          alert('Falha ao confirmar a reserva. Tente novamente.');
+        }
+      },
+      error: (err) => {
+        console.error('Erro ao confirmar a reserva:', err);
+        alert('Falha ao confirmar a reserva. Tente novamente.');
+      },
+    });
+  }
 }
