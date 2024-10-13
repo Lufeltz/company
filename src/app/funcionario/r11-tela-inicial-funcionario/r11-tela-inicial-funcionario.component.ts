3615ddd0f509951d89fc1bde4f5e3a4e8c112c05
Julio Yamaguchi <julioyamaguchi@ufpr.br>
2024-10-13 04:09:07 -0300

    r11 integrado com jsonserver

--- src/app/funcionario/r11-tela-inicial-funcionario/r11-tela-inicial-funcionario.component.ts
+++ src/app/funcionario/r11-tela-inicial-funcionario/r11-tela-inicial-funcionario.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { VoosService } from '../../services/prototipo/voos.service';
 import { CommonModule } from '@angular/common';
+import { Voo } from '../../shared/models/prototipo/voo.model';
 
 @Component({
   selector: 'app-r11-tela-inicial-funcionario',
@@ -9,6 +10,27 @@ import { CommonModule } from '@angular/common';
   templateUrl: './r11-tela-inicial-funcionario.component.html',
   styleUrl: './r11-tela-inicial-funcionario.component.css',
 })
-export class R11TelaInicialFuncionarioComponent {
-  constructor(private voos: VoosService) {}
+export class R11TelaInicialFuncionarioComponent implements OnInit {
+  public voos: Voo[] = [];
+
+  constructor(private voosService: VoosService) {}
+
+  ngOnInit(): void {
+    this.getAllvoos();
+  }
+
+  getAllvoos() {
+    this.voosService.getAllVoos().subscribe({
+      next: (data: Voo[] | null) => {
+        if (data == null) {
+          this.voos = [];
+        } else {
+          this.voos = data;
+        }
+      },
+      error: (err) => {
+        console.log('Erro ao carregar voos');
+      },
+    });
+  }
 }
